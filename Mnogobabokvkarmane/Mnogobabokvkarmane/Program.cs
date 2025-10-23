638fee47dbfdc7f4b70ad2671832cd1c4a3d3147
shzyy <samsonov.eugene@bk.ru>
2025-10-23 09:10:42 +0300

    etot kod stoil mne lyam
    3012

---
+++ Mnogobabokvkarmane/Mnogobabokvkarmane/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Mnogobabokvkarmane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta numbrid");
+
+            //genereerib iga kord suvalised numbrid
+            //1-st kuni 6-ni
+
+            //peate kasutama switchi, et igat numbrit näidata
+
+            int number = new Random().Next(1, 6);
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("sisestasid number 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("sisestasid number 2");
+                    break;
+
+                case 3:
+                    Console.WriteLine("sisestasid number 3");
+                    break;
+
+                case 4:
+                    Console.WriteLine("sisestasid number 4");
+                    break;
+
+                case 5:
+                    Console.WriteLine("sisestasid number 5");
+                    break;
+
+                case 6:
+                    Console.WriteLine("sisestasid number 6");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
